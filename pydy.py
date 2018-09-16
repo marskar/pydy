62253b67e44348043d9c2e0bffe8ff31f68bb47a
Martin Skarzynski <marskar@gmail.com>
2018-09-15 21:43:20 -0400

    add tests for np.chararray

--- pydy.py
+++ pydy.py
@@ -3,15 +3,20 @@ from helper import *
 import __main__
 
 
-def pydy(names: Iterable[str], target: str = 'pd.DataFrame') -> None:
+def pydy(add: Iterable[str], cls: str = 'pd.DataFrame') -> None:
 
-    def add_method(name, targ=target):
-        main_target = '__main__.' + targ
+    def add_method(name, target=cls):
+        main_target = '__main__.' + target
         try:
             if eval(main_target):
                     setattr(eval(main_target), name, eval(name))
         except AttributeError as e:
             print(e)
 
-    any(map(add_method, names))
-
+    if type(add) == str:
+        add_method(add)
+    elif type(add) in (list, tuple):
+        any(map(add_method, add))
+    else:
+        print(f'The add argument was {type(names)}. '
+              'It can only be a list/tuple of strings or a string')
--- pydytest.py
+++ pydytest.py
@@ -1,14 +1,15 @@
 import pandas
 import pandas as pd
 from pandas import DataFrame
+import numpy as np
 from pydy import pydy
 
-pydy(['say_hi', 'say_moo'])
+pydy(add=['say_hi', 'say_moo'], cls='np.chararray')
+pydy(add='say_hi', cls='pd.DataFrame')
+
+np.chararray.say_hi()
+np.chararray.say_moo()
 
 pd.DataFrame.say_hi()
 pandas.DataFrame.say_hi()
 DataFrame.say_hi()
-
-pd.DataFrame.say_moo()
-pandas.DataFrame.say_moo()
-DataFrame.say_moo()
