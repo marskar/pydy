911b93cb929836e4e4a68d88386d087d46bd903a
Martin Skarzynski <marskar@gmail.com>
2018-09-15 21:13:39 -0400

    refactor code

--- pydy.py
+++ pydy.py
@@ -3,23 +3,15 @@ from helper import *
 import __main__
 
 
-def pydy(names: Iterable[str]) -> None:
+def pydy(names: Iterable[str], target: str = 'pd.DataFrame') -> None:
 
+    def add_method(name, targ=target):
+        main_target = '__main__.' + targ
         try:
-            if __main__.pd.DataFrame:
-                for name in names:
-                    setattr(__main__.pd.DataFrame, name, eval(name))
-        except AttributeError:
-            pass
-        try:
-            if __main__.pandas.DataFrame:
-                for name in names:
-                    setattr(__main__.pandas.DataFrame, name, eval(name))
-        except AttributeError:
-            pass
-        try:
-            if __main__.DataFrame:
-                for name in names:
-                    setattr(__main__.DataFrame, name, eval(name))
-        except AttributeError:
-            pass
+            if eval(main_target):
+                    setattr(eval(main_target), name, eval(name))
+        except AttributeError as e:
+            print(e)
+
+    any(map(add_method, names))
+
