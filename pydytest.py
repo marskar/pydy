b9ce9e26ba52fe85b17711e9f32748b743ef64cc
Martin Skarzynski <marskar@gmail.com>
2018-09-21 17:49:43 -0400

    added tests

--- pydytest.py
+++ pydytest.py
@@ -7,6 +7,15 @@ PydyFrame = pydy(cls=pd.DataFrame, src='helper')
 # instantiate it
 df = PydyFrame(data=pd.read_csv('risk_factors_cervical_cancer.csv'))
 
-# test out methods added from helper file
+# test methods added from helper file
 df.say_hi()
 df.say_moo()
+
+# test method from parent class
+df.head(n=1)
+
+# confirm that df is an instance of pd.DataFrame and PydyFrame
+isinstance(df, (pd.DataFrame, PydyFrame))
+
+# confirm that PydyFrame is a subclass of pd.DataFrame
+issubclass(PydyFrame, pd.DataFrame)
