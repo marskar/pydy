89c2150b01886251820d1546b5b4d19aaf5232ad
Martin Skarzynski <marskar@gmail.com>
2018-09-21 17:23:35 -0400

    i fixed it

--- pydy.py
+++ pydy.py
@@ -1,31 +1,25 @@
 from importlib import import_module
 import pickle
-import os.path
-
-if os.path.isfile('helper.py'):
-    from helper import *
 
 
-def pydy(cls, arg=None, src=None):
+def pydy(cls, arg=None, src: str = 'helper'):
 
-    class Pydy:#(cls):
+    class Pydy(cls):
         def save(self, filename='pydy.pickle'):
             with open(filename, 'wb') as f:
                 # Pickle Pydy class using the highest protocol available.
                 pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
 
+    mod = import_module(src)
+    internals = mod.__dict__.items()
+    method_names = [name for name, val in internals if callable(val)]
+
+    # change this loop to map call?
+    for name in method_names:
+        met = getattr(mod, name)
+        setattr(Pydy, name, met)
+
     if arg is not None:
-        instance = Pydy(arg)
+        return Pydy(arg)
     else:
-        instance = Pydy
-
-    if src is not None:
-        if src.endswith('.py'):
-            src = src[:-3]
-        mod = import_module(src)
-        internals = mod.__dict__.items()
-        method_names = [name for name, val in internals if callable(val)]
-
-        for name in method_names:
-            instance.name = name
-    return instance
+        return Pydy
--- pydytest.py
+++ pydytest.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from pydy import pydy
 
+df = pydy(cls=pd.DataFrame, arg=pd.read_csv('risk_factors_cervical_cancer.csv'), src='helper')
+
+df = pydy(cls=pd.DataFrame, src='helper')
+df.save()
+df.say_hi()
+
 df = (pydy(cls=pd.DataFrame,
            arg=pd.read_csv('risk_factors_cervical_cancer.csv'),
            src='helper.py')
-    .dropna()
-    .head()
+      .dropna()
+      .head()
  )
 
 df.save()
+df.say_hi()
 
 np.chararray.say_hi()
 np.chararray.say_moo()
