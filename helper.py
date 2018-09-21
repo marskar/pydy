0c986608c831882689720dc8d186b20725d1db1e
Martin Skarzynski <marskar@gmail.com>
2018-09-21 17:39:41 -0400

    added comments

--- helper.py
+++ helper.py
@@ -1,6 +1,5 @@
 def say_hi(self):
     print("Hi!")
 
-
 def say_moo(self):
     print("Moo!")
--- pydytest.py
+++ pydytest.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from pydy import pydy
 
+# create new class
 PydyFrame = pydy(cls=pd.DataFrame, src='helper')
 
+# instantiate it
 df = PydyFrame(data=pd.read_csv('risk_factors_cervical_cancer.csv'))
 
+# test out methods added from helper file
 df.say_hi()
 df.say_moo()
-df.unused()
-
-df.save()
-df.say_hi()
